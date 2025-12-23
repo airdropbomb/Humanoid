a7769cad773bb1818a4e33f470f0686b4c71f6e9
vonssy <kak.reyvaldizakaria@gmail.com>
2025-12-23 21:07:49 +0700

    Delete Captcha Solver

--- bot.py
+++ bot.py
@@ -681,11 +681,13 @@ class Humanoid:
                     await self.load_proxies()
 
                 separator = "=" * 25
-                for account in accounts:
+                for idx, account in enumerate(accounts, start=1):
                     if account:
                         address = self.generate_address(account)
                         self.log(
                             f"{Fore.CYAN + Style.BRIGHT}{separator}[{Style.RESET_ALL}"
+                            f"{Fore.WHITE + Style.BRIGHT} {idx} {Style.RESET_ALL}"
+                            f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"
                             f"{Fore.WHITE + Style.BRIGHT} {self.mask_account(address)} {Style.RESET_ALL}"
                             f"{Fore.CYAN + Style.BRIGHT}]{separator}{Style.RESET_ALL}"
                         )
